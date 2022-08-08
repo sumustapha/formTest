64ab0f35fca35642a5074f8d014525f7092e1bc3
sumustapha <sumustapha@gmail.com>
2022-08-08 20:29:45 +0100

    v2

---
+++ app.js
@@ -0,0 +1,36 @@
+const formSubmit = document.querySelector('#form')
+const password = document.querySelector('#password')
+const confirmp = document.querySelector('#confirm')
+const error = document.querySelector('.error')
+
+
+formSubmit.addEventListener('submit',(e)=>{
+console.log('enter')
+    e.preventDefault()
+    checkPassword()
+})
+
+function checkPassword(){
+    const passwordValue = password.value.trim()
+    const confirmpValue = confirmp.value.trim()
+
+    if((passwordValue || confirmpValue)===""){
+        error.style.display = "block"
+        error.innerText = "Password cannot be empty"
+        password.className = 'errors'
+        confirmp.className = 'errors'
+    }
+    else if(passwordValue!==confirmpValue){
+        error.style.display = "block"
+        error.innerText = "Password does not match"
+        password.className = 'errors'
+        confirmp.className = 'errors'
+    }
+    else{
+        error.style.display = "none"
+        error.innerText = ""
+        password.className = ''
+        confirmp.className = ''
+    }
+
+}
